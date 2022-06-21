f6716d2931b3b79acf2c06bafffc9f7f4d76be1e
SamuelTTW <espaitk@gmail.com>
2022-06-21 03:13:29 -0400

    fix ESLint error

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-const line1__bars = document.querySelector(".line1__bars-menu");
-const line2__bars = document.querySelector(".line2__bars-menu");
-const line3__bars = document.querySelector(".line3__bars-menu");
-const list__bars = document.querySelector(".list_bar-menu");
+const line1__Bars = document.querySelector('.line1__bars-menu');
+const line2__Bars = document.querySelector('.line2__bars-menu');
+const line3__Bars = document.querySelector('.line3__bars-menu');
+const list__Bars = document.querySelector('.list_bar-menu');
 
-document.querySelector('#bmenu').addEventListener("click", () => {
-  line1__bars.classList.toggle('activeline1__bars-menu');
-  line2__bars.classList.toggle('activeline2__bars-menu');
-  line3__bars.classList.toggle('activeline3__bars-menu');
-  list__bars.classList.toggle('deploy');
+document.querySelector('#bmenu').addEventListener('click', () => {
+  line1__Bars.classList.toggle('activeline1__bars-menu');
+  line2__Bars.classList.toggle('activeline2__bars-menu');
+  line3__Bars.classList.toggle('activeline3__bars-menu');
+  list__Bars.classList.toggle('deploy');
 });
-document.querySelector('.a0').addEventListener("click", () => {
-  line1__bars.classList.toggle('activeline1__bars-menu');
-  line2__bars.classList.toggle('activeline2__bars-menu');
-  line3__bars.classList.toggle('activeline3__bars-menu');
-  list__bars.classList.toggle('deploy');
+document.querySelector('.a0').addEventListener('click', () => {
+  line1__Bars.classList.toggle('activeline1__bars-menu');
+  line2__Bars.classList.toggle('activeline2__bars-menu');
+  line3__Bars.classList.toggle('activeline3__bars-menu');
+  list__Bars.classList.toggle('deploy');
 });
-
-
