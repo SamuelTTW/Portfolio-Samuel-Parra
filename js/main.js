5097def80f9c164b695fcb92b28a98f2aeebedae
SamuelTTW <espaitk@gmail.com>
2022-06-21 03:18:45 -0400

    fix Eslint error

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-const line1__Bars = document.querySelector('.line1__bars-menu');
-const line2__Bars = document.querySelector('.line2__bars-menu');
-const line3__Bars = document.querySelector('.line3__bars-menu');
-const list__Bars = document.querySelector('.list_bar-menu');
+const lBars1 = document.querySelector('.line1__bars-menu');
+const lBars2 = document.querySelector('.line2__bars-menu');
+const lBars3 = document.querySelector('.line3__bars-menu');
+const listBars = document.querySelector('.list_bar-menu');
 
 document.querySelector('#bmenu').addEventListener('click', () => {
-  line1__Bars.classList.toggle('activeline1__bars-menu');
-  line2__Bars.classList.toggle('activeline2__bars-menu');
-  line3__Bars.classList.toggle('activeline3__bars-menu');
-  list__Bars.classList.toggle('deploy');
+  lBars1.classList.toggle('activeline1__bars-menu');
+  lBars2.classList.toggle('activeline2__bars-menu');
+  lBars3.classList.toggle('activeline3__bars-menu');
+  listBars.toggle('deploy');
 });
 document.querySelector('.a0').addEventListener('click', () => {
-  line1__Bars.classList.toggle('activeline1__bars-menu');
-  line2__Bars.classList.toggle('activeline2__bars-menu');
-  line3__Bars.classList.toggle('activeline3__bars-menu');
-  list__Bars.classList.toggle('deploy');
+  lBars1.classList.toggle('activeline1__bars-menu');
+  lBars2.classList.toggle('activeline2__bars-menu');
+  lBars3.classList.toggle('activeline3__bars-menu');
+  listBars.toggle('deploy');
 });
