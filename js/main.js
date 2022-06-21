6508e32fe6f472905af5fcbe84587e0cbbb03182
SamuelTTW <espaitk@gmail.com>
2022-06-21 09:29:24 -0400

    Fix bugs

--- js/main.js
+++ js/main.js
@@ -2,29 +2,35 @@ const lBars1 = document.querySelector('.line1__bars-menu');
 const lBars2 = document.querySelector('.line2__bars-menu');
 const lBars3 = document.querySelector('.line3__bars-menu');
 const listBars = document.querySelector('.list_bar-menu');
+const bMenu = document.querySelector('#bmenu');
+const xMenu = document.querySelector('.xmenu');
 
 document.querySelector('#bmenu').addEventListener('click', () => {
-  lBars1.classList.toggle('activeline1__bars-menu');
-  lBars2.classList.toggle('activeline2__bars-menu');
-  lBars3.classList.toggle('activeline3__bars-menu');
-  listBars.toggle('deploy');
+  listBars.classList.toggle('deploy');
+  bMenu.classList.toggle('none');
+  xMenu.classList.toggle('display');
 });
+
+document.querySelector('.xmenu').addEventListener('click', () => {
+  listBars.classList.toggle('deploy');
+  bMenu.classList.toggle('none');
+  xMenu.classList.toggle('display');
+});
+
 document.querySelector('.a0').addEventListener('click', () => {
-  lBars1.classList.toggle('activeline1__bars-menu');
-  lBars2.classList.toggle('activeline2__bars-menu');
-  lBars3.classList.toggle('activeline3__bars-menu');
-  listBars.toggle('deploy');
+  listBars.classList.toggle('deploy');
+  bMenu.classList.toggle('none');
+  xMenu.classList.toggle('display');
 });
 
 document.querySelector('.a1').addEventListener('click', () => {
-  lBars1.classList.toggle('activeline1__bars-menu');
-  lBars2.classList.toggle('activeline2__bars-menu');
-  lBars3.classList.toggle('activeline3__bars-menu');
-  listBars.toggle('deploy');
+  listBars.classList.toggle('deploy');
+  bMenu.classList.toggle('none');
+  xMenu.classList.toggle('display');
 });
+
 document.querySelector('.a2').addEventListener('click', () => {
-  lBars1.classList.toggle('activeline1__bars-menu');
-  lBars2.classList.toggle('activeline2__bars-menu');
-  lBars3.classList.toggle('activeline3__bars-menu');
-  listBars.toggle('deploy');
+  listBars.classList.toggle('deploy');
+  bMenu.classList.toggle('none');
+  xMenu.classList.toggle('display');
 });
