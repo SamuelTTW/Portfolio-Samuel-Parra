c84038634ac1d19d409246bb7593aea7777df44b
SamuelTTW <espaitk@gmail.com>
2022-06-22 09:10:00 -0400

    add more efective Function Hamburger

--- js/main.js
+++ js/main.js
@@ -4,42 +4,15 @@ const xMenu = document.querySelector('.xmenu');
 const mHeader = document.querySelector('.Mheader');
 const pop = document.querySelector('.popup-window');
 
-document.querySelector('#bmenu').addEventListener('click', () => {
+function Hamburger() {
   listBars.classList.toggle('deploy');
   bMenu.classList.toggle('none');
   xMenu.classList.toggle('display');
   mHeader.classList.toggle('none');
   pop.classList.toggle('popup-show');
-});
-
-document.querySelector('.xmenu').addEventListener('click', () => {
-  listBars.classList.toggle('deploy');
-  bMenu.classList.toggle('none');
-  xMenu.classList.toggle('display');
-  mHeader.classList.toggle('none');
-  pop.classList.toggle('popup-show');
-});
-
-document.querySelector('.a0').addEventListener('click', () => {
-  listBars.classList.toggle('deploy');
-  bMenu.classList.toggle('none');
-  xMenu.classList.toggle('display');
-  mHeader.classList.toggle('none');
-  pop.classList.toggle('popup-show');
-});
-
-document.querySelector('.a1').addEventListener('click', () => {
-  listBars.classList.toggle('deploy');
-  bMenu.classList.toggle('none');
-  xMenu.classList.toggle('display');
-  mHeader.classList.toggle('none');
-  pop.classList.toggle('popup-show');
-});
-
-document.querySelector('.a2').addEventListener('click', () => {
-  listBars.classList.toggle('deploy');
-  bMenu.classList.toggle('none');
-  xMenu.classList.toggle('display');
-  mHeader.classList.toggle('none');
-  pop.classList.toggle('popup-show');
-});
+}
+document.querySelector('#bmenu').addEventListener('click', Hamburger);
+document.querySelector('.xmenu').addEventListener('click', Hamburger);
+document.querySelector('.a0').addEventListener('click', Hamburger);
+document.querySelector('.a1').addEventListener('click', Hamburger);
+document.querySelector('.a2').addEventListener('click', Hamburger );
