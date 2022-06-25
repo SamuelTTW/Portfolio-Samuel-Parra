fdf128f091872e119e268f07a27bc75e123c5bf8
SamuelTTW <espaitk@gmail.com>
2022-06-25 04:40:51 -0400

    Custodio Commit "add function "red" "

---
+++ js/Formulary-validation.js
@@ -0,0 +1,24 @@
+const formulary = document.querySelector('#data');
+const emailInput = document.querySelector('#emailfield');
+const pemail = document.querySelector('.pemail');
+const expre = {
+  regexemail: /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/,
+};
+
+const red = () => {
+  if(expre.regexemail.test(emailInput.value)){
+    emailInput.classList.remove('field1');
+    pemail.classList.remove('lowercase');
+  }
+  else {
+    emailInput.classList.add('field1');
+    pemail.classList.add('lowercase');
+  }
+};
+
+emailInput.addEventListener('keyup', red);
+emailInput.addEventListener('blur', red);
+
+formulary.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
