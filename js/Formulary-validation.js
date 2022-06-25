4a45c0701c4dcbe238c8cfbbdd336f3d47bf7c21
SamuelTTW <espaitk@gmail.com>
2022-06-25 05:46:33 -0400

    Fix bug

--- js/Formulary-validation.js
+++ js/Formulary-validation.js
@@ -1,17 +1,24 @@
 const formulary = document.querySelector('#data');
 const emailInput = document.querySelector('#emailfield');
 const pemail = document.querySelector('.pemail');
+const send = document.querySelector('.pbutton');
 const expre = {
   regexemail: /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/,
 };
 
+let lowercaseEmail = true
+
+
 const red = () => {
+
   if (expre.regexemail.test(emailInput.value)) {
     emailInput.classList.remove('field1');
     pemail.classList.remove('lowercase');
+    lowercaseEmail = true;
   } else {
     emailInput.classList.add('field1');
     pemail.classList.add('lowercase');
+    lowercaseEmail = false;
   }
 };
 
@@ -19,5 +26,16 @@ emailInput.addEventListener('keyup', red);
 emailInput.addEventListener('blur', red);
 
 formulary.addEventListener('submit', (e) => {
-  e.preventDefault();
+
+  if (lowercaseEmail) {
+    send.classList.add('send');
+    setTimeout(() => {
+      send.classList.remove('send');
+    }, 5000);
+  }
+  else {
+    e.preventDefault();
+  }
 });
+
+
